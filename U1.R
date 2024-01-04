4f6a855eacb0b89846b7e7f19eeb710ea0b51cf8
Yufang <411422259@m365.fju.edu.tw>
2024-01-04 18:47:14 +0800

    線代解題 單元一 1-6~1-12

---
+++ U1.R
@@ -0,0 +1,10 @@
+# 1-6 1-7
+A <- matrix(c(1,2,2,1,3,4), nrow =2 , ncol = 3)
+B <- matrix(c(1,2,3,0,1,2), nrow =3 , ncol = 2)
+C <- matrix(c(3,4,2,-1,1,1,3,5,3), nrow =3 , ncol = 3)
+D <- matrix(c(3,2,-2,4), nrow =2 , ncol = 2)
+E <- matrix(c(2,0,3,-4,1,2,5,4,1), nrow =3 , ncol = 3)
+F <- matrix(c(-4,5,2,3), nrow =2 , ncol = 2)
+O <- matrix(c(0,0,0,0,0,0,0,0,0), nrow =3 , ncol = 3)
+
+t(A)
